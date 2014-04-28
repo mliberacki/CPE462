5a294da7a6efb96c8b74d0867dabdfb318036d9f
mliberacki <marissaliberacki@yahoo.com>
2014-04-28 18:40:48 -0400

    Marissa fixed the facial tracking

--- facial_tracking.m
+++ facial_tracking.m
@@ -1,23 +1,32 @@
+%Facial Tracking and Suposition
+
 clear all; close all;
 
-faceimage = imread('face.jpg');
-pic = rgb2ycbcr(faceimage); %RGB -> YCbCr conversion
+raw_image = imread('face3.jpg'); %Read image
+raw_image_adj = rgb2ycbcr(raw_image); %RGB -> YCbCr conversion
 
 %Remove noise from the pic by blurring with a gaussian filter
 G = fspecial('gaussian',[5 5],2);
-pic = imfilter(pic,G,'same');
+raw_image_adj = imfilter(raw_image_adj,G,'same');
 
-%Split into Y, Cr, Cb components
-y = size(pic,2);
-x = size(pic,1);
-Y=zeros(x,y); Cb=zeros(x,y); Cr=zeros(x,y);   % initialization;
-Y=pic(:,:,1); Cb=pic(:,:,2); Cr=pic(:,:,3);
+y = size(raw_image_adj,2);
+x = size(raw_image_adj,1);
+
+%Initialize YCbCr values
+Y=zeros(x,y); Cb=zeros(x,y); Cr=zeros(x,y);
+Y=raw_image_adj(:,:,1); Cb=raw_image_adj(:,:,2); Cr=raw_image_adj(:,:,3);
+
+%Initialize RGB values
+R=zeros(x,y); G=zeros(x,y); B=zeros(x,y);
+R=raw_image(:,:,1); G=raw_image(:,:,2); B=raw_image(:,:,3);
+R = double(R); G = double(G); B=double(B);
 
 %Convert from uint8->double to work with the pixels
 Y = double(Y);
 Cb = double(Cb);
 Cr = double(Cr);
 
+%Define new variables
 new_Cb = Cb;
 new_Cr = Cr;
 new_Y = zeros(x,y);
@@ -26,70 +35,210 @@ new_Y = zeros(x,y);
 %Turn skincolored things white, background black
 for i=1:size(Y,1)
     
-   for k=1:size(Y,2)
-       
-       Y_val = Y(i,k);
-       Cb_val = Cb(i,k);
-       Cr_val = Cr(i,k);
-       
-       if Cb_val>76 && Cb_val<165 
-           new_Cb_val = 256;
-       else
-           new_Cb_val = 0;
-       end
-       
-       if Cr_val>130 && Cr_val<195
-           new_Cr_val = 256;
-       else
-           new_Cr_val = 0;
-       end
-       
-       new_Cb(i,k) = new_Cb_val;
-       new_Cr(i,k) = new_Cr_val;
-       
-   end
+    for k=1:size(Y,2)
+        
+        Y_val = Y(i,k);
+        Cb_val = Cb(i,k);
+        Cr_val = Cr(i,k);
+        
+        if Cb_val>=77 && Cb_val<=135
+            new_Cb_val = 256;
+        else
+            new_Cb_val = 0;
+            G(i,k) = 0;
+            R(i,k) = 0;
+            B(i,k) = 0;
+        end
+        
+        if Cr_val>=130 && Cr_val<=190
+            new_Cr_val = 256;
+        else
+            new_Cr_val = 0;
+            B(i,k)=0;
+            R(i,k) = 0;
+            G(i,k) = 0;
+        end
+        
+        new_Cb(i,k) = new_Cb_val;
+        new_Cr(i,k) = new_Cr_val;
+        
+    end
 end
 
+%New segmented image
 image = cat(3,uint8(new_Y),uint8(new_Cb),uint8(new_Cr));
+
+%Convert it to black and white
+bl_image = im2bw(image,.6);
 imshow(im2bw(image,.6))
-%imshow(image);
+
+%Make an image that blacks out all non-skin colored things
+seg_image_rgb = cat(3,uint8(R),uint8(G),uint8(B));
+
+
+%Current algorithm also picks up some hair
+%Also the algorithm picks up some unnecessary things
+
+%Crop out picture where ratio of black to white is too high
+
+%Get number of zeros and ones in the image
+zeros_bl = bl_image==0;
+ones_bl = bl_image~=0;
+zero = sum(zeros_bl,2);
+one = sum(ones_bl,2);
+
+ratio_vect = zeros(size(zero,1),1);
+
+%Get the black to white ratio for each row
+for i=1:length(zero)
+    
+    numzeros = zero(i);
+    numones = one(i);
+    
+    ratio = numzeros / numones;
+    ratio_vect(i) = ratio;
+    
+end
+
+%My way to adjust for picking up so much hair
+%Just cutoff the parts where there isn't as much white
+%Usually towards the top of the head
+mean_ratio = mean(~isinf(ratio_vect(:,1)));
+std_ratio = std(~isinf(ratio_vect(:,1)));
+highest_ratio = mean_ratio + std_ratio;
+
+%Initialize RGB values
+y = size(seg_image_rgb,2);
+x = size(seg_image_rgb,1);
+R=zeros(x,y); G=zeros(x,y); B=zeros(x,y);
+R=seg_image_rgb(:,:,1); G=seg_image_rgb(:,:,2); B=seg_image_rgb(:,:,3);
+R = double(R); G = double(G); B=double(B);
+
+%Loop through the image and make parts black for small black:white 
+for i=1:size(R,1)
+    
+    if ratio_vect(i)>highest_ratio  || isinf(ratio_vect(i))
+        for k=1:size(R,2)
+            
+            G(i,k) = 0;
+            R(i,k) = 0;
+            B(i,k) = 0;
+            
+        end
+    end 
+end
+
+
+seg_image_rgb2 = cat(3,uint8(R),uint8(G),uint8(B));
+figure; imshow(seg_image_rgb2);
+bl_image2 = im2bw(seg_image_rgb2,.6);
+
+%Use canny edge detection on the image to pic up facial features
+%This threshold of .3 worked the best for us
+edge_image = edge(rgb2gray(seg_image_rgb2),'canny',.3);
+figure; imshow(edge_image);
+
+%Facial feature detection
+
+count = 0;
+index = 0;
+
+%Loop through image and get the first row of white pixel
+%This is the top of the head
+for i=1:size(edge_image,1)
+    
+    for j=1:size(edge_image,2)
+        
+        if edge_image(i,j)==1
+            white_data = edge_image(i,:);
+            
+            if count==0
+                index = i;
+                face_top_array = white_data;                   
+            end
+            count = count +1;          
+        end        
+    end   
+end
+
+
+count_ones = 0;
+start_col = 0;
+
+%Get column index where the ones start
+for i=1:length(face_top_array)
+   
+    if face_top_array(1,i)==1
+        
+        if start_col==0
+            start_col = i;
+        end
+        
+        count_ones = count_ones + 1;
+    
+    end
+    
+end
+
+%The middle of the top of the face
+mid_face_index = round(start_col + count_ones/2);
+mid_top_face = [index,mid_face_index];
+
+%Get image centroids - get the centroid of the face
+s  = regionprops(bl_image, 'centroid');
+centroids = cat(1, s.Centroid);
+imshow(bl_image);hold on;
+plot(centroids(:,1), centroids(:,2), 'b*');
+
+centroid_range1 = round(start_col + count_ones/4);
+centroid_range2 = round(mid_face_index + 2.5*count_ones/2);
+
+face_cen_vect = [];
+
+for i=1:size(centroids,1)
+    
+    if centroids(i,1) > centroid_range1 && centroids(i,1) < centroid_range2
+        
+        if centroids(i,2) > mid_face_index+10
+            face_cen_vect = vertcat(centroids(i,:),face_cen_vect);
+        
+        end
+        
+        
+        
+    end
+    
+end
+
+if size(face_cen_vect,1) > 1
+    %Pick centroid with point closest to col index of mid face top
+    dist = face_cen_vect(:,2) - mid_top_face(:,2);
+    
+    smallest_point = min(dist);
+    
+    for i=1:length(dist)
+        if dist(i)==smallest_point
+           face_centroid =  face_cen_vect(i,:);
+        end
+    end
+elseif size(face_cen_vect,1)==1
+    face_centroid = face_cen_vect(1,:);
+end
 
 
 
+dist_btwn_top_center = face_centroid(:,2) - mid_top_face(:,1);
 
-% dx1=edge(pic(:,:,1),'canny');
-% dx1=(dx1*255);
-% img2(:,:,1)=dx1;
-% img2(:,:,2)=pic(:,:,2);
-% img2(:,:,3)=pic(:,:,3);
-% rslt=ycbcr2rgb(uint8(img2));
-% imshow(rslt);
-% 
-% 
-% rslt = rgb2gray(rslt);
-% K = filter2(fspecial('average',3),rslt)/255;
-% %mshow(K)
-% %figure,imshow(rslt);
-% 
-% %K is a greyscale sparse matrix
-% 
-% %Loop through K row by row and find where there are large rows of zeros
-% 
-% split_image = (size(K,2))/2;
-% count = 0;
-% 
-% for i=1:size(K,1)
-%     
-%     %Find where there are zeros
-%     row_zeros = find(K(i,:));
-%     
-%     consectutive=0;
-%    
-%     if size(row_zeros,2)>=split_image
-%     
-%        count = count+1;
-%     end
-% 
-%     %What i think i want to do now is try to find the biggest consecutive
-%     %row of indicies
-% end
+mid_bottom_face = [ face_centroid(1,2)+dist_btwn_top_center/2 , mid_face_index];
+    %face_centroid(1,2)+index , mid_face_index];
+
+imshow(seg_image_rgb2);
+hold on;
+plot(mid_bottom_face(:,2), mid_bottom_face(:,1), 'b*');
+hold on; 
+plot(mid_top_face(:,2), mid_top_face(:,1), 'b*');
+hold on;
+plot(face_centroid(:,1), face_centroid(:,2), 'b*');
+hold off;
+
+
